881490c40442b9019e5f3d427e960d097c728c59
kuro_ye <erhanaket77@gmail.com>
2023-10-01 17:58:42 +0600

    Lottery

--- learning_vue/src/pages/LotteryPage.vue
+++ learning_vue/src/pages/LotteryPage.vue
@@ -1,8 +1,27 @@
 <template>
-    <h1>I AM LOTTERY</h1>
+    <div class="new-todo-btn">
+    <router-link to="/todo-add">
+      <el-button :icon="CirclePlus">New Card</el-button>
+    </router-link>
+    </div>
+
     <CardList/>
 </template>
 
 <script setup>
  import CardList from '@/components/lottery/CardList.vue'
+import { CirclePlus } from "@element-plus/icons-vue";
+
 </script>
+
+
+<style scoped>
+
+.new-todo-btn {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transform: translate(0, 50px);
+}
+
+</style>
